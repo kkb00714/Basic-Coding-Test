ea731e262acfc8a8a3be6551706d34d86ac7ffc7
kkb00714 <66295746+kkb00714@users.noreply.github.com>
2023-11-01 16:25:38 +0900

    [level unrated] Title: 5명씩, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.py
@@ -0,0 +1,13 @@
+def solution(names): # names = 걍 문자열임
+    answer = []
+    attraction = []
+    
+    
+    for i in range(0, len(names), 5):
+        attraction.append(names[i:i+5])
+        
+    for j in range(len(attraction)):
+        answer.append(attraction[j][0])
+    
+    
+    return answer
