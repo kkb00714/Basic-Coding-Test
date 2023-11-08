cde766cff905088c0db37dec2c68e8ab04231b88
kkb00714 <66295746+kkb00714@users.noreply.github.com>
2023-11-08 09:53:47 +0900

    [level unrated] Title: ad 제거하기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,8 @@
+def solution(strArr):
+    answer = [s for s in strArr if 'ad' not in s]
+    # 리스트 컴프리헨션
+    # 여기서 s 는 각 리스트의 요소인 문자열을 의미
+    # for s in strArr => strArr 리스트를 순회하며 각 요소를 's'에 대입함        
+    # if 'ad' no in s => 부분문자열 'ad'가 문자열 's' 에 없을 경우, 해당 문자열을 선택함
+    
+    return answer
