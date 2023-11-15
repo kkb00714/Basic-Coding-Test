68c96d93f7c12b65d3bc2fbeb54428ac000b21f9
kkb00714 <66295746+kkb00714@users.noreply.github.com>
2023-11-15 09:53:53 +0900

    [level unrated] Title: x 사이의 개수, Time: 3.77 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    answer = []
+    
+    myString = myString.split('x')
+    
+    for i in range(len(myString)):
+        answer.append(len(myString[i]))
+    
+    return answer
